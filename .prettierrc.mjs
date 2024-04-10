0853882e4ef72fcfd376287faee5e84ebba0f114
wonseok-han <oshan1112@gmail.com>
2024-04-10 20:13:00 +0900

    astro prettier 적용

---
+++ .prettierrc.mjs
@@ -0,0 +1,25 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+  embeddedLanguageFormatting: 'auto',
+};
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = `wonseok-han's Blog`;
+export const SITE_DESCRIPTION = `Welcome to wonseok-han's Blog!`;
