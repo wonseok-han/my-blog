8aa419aa6a9f5f89833761a103101e9ae23ce30a
wonseok-han <oshan1112@gmail.com>
2024-04-15 12:48:48 +0900

    blog 작업중

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        top: '0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)',
+        'top-dark':
+          '0 -4px 6px -1px rgba(128, 128, 128, 0.1), 0 -2px 4px -1px rgba(128, 128, 128, 0.06)',
+      },
+      colors: {
+        dark: {},
+      },
+    },
+  },
+  variants: {
+    extend: {
+      boxShadow: ['dark'],
+    },
   },
   plugins: [],
+  darkMode: 'selector',
 };
