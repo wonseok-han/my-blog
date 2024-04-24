cde1d90388f69574c8bb1e911315acf00ea825ba
wonseok-han <oshan1112@gmail.com>
2024-04-24 23:19:00 +0900

    다크모드 적용

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,20 +3,20 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      boxShadow: {
-        top: '0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)',
-        'top-dark':
-          '0 -4px 6px -1px rgba(128, 128, 128, 0.1), 0 -2px 4px -1px rgba(128, 128, 128, 0.06)',
-      },
       colors: {
-        dark: {},
+        'theme-background': {
+          DEFAULT: '#ffffff', // 기본 배경색
+          dark: '#1a202c', // 다크 모드에서의 배경색
+        },
+        'theme-text': {
+          DEFAULT: '#1f2937', // 기본 텍스트 색상
+          dark: '#ffffff', // 다크 모드에서의 텍스트 색상
+        },
       },
     },
   },
   variants: {
-    extend: {
-      boxShadow: ['dark'],
-    },
+    extend: {},
   },
   plugins: [],
   darkMode: 'selector',
