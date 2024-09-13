79bf697acf22745b2c443b347bb5bfb722924ad5
wonseok-han <oshan1112@gmail.com>
2024-09-13 10:14:56 +0900

    input에 애니메이션 추가

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -1,51 +1,82 @@
 'use client';
 
 import { useSearchStore } from '@/store/search-store';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const SearchInput = () => {
   const { searchInput, setSearchInput, searchTrigger } = useSearchStore();
   const [value, setValue] = useState(searchInput || '');
+  const [isInputShow, setIsInputShow] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     setValue(searchInput || '');
     searchTrigger();
   }, [searchInput]);
 
+  useEffect(() => {
+    if (isInputShow && inputRef?.current) {
+      // 애니메이션이 완료된 후 포커스를 설정하기 위해 약간의 지연시간을 둠
+      setTimeout(() => {
+        inputRef.current?.focus();
+      }, 300); // 300ms는 애니메이션의 지속 시간에 맞게 조절 가능
+    }
+  }, [isInputShow]);
+
   return (
-    <div className="relative">
-      <input
-        className="w-60 h-10 border rounded-lg text-sm font-light pl-2 pr-8 py-1 dark:bg-nosferatu-900"
-        value={value}
-        onChange={(event) => {
-          setValue(event.target.value);
-        }}
-        onKeyDown={(event) => {
-          if (event.key === 'Enter') {
-            setSearchInput(value);
-          }
-        }}
-      />
-      <span
-        className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer"
-        onClick={() => setSearchInput(value)}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-5"
+    <>
+      <div className={`relative`}>
+        <input
+          onFocus={() => console.log('포커스')}
+          ref={inputRef}
+          className={`w-60 h-10 border rounded-lg text-sm font-light pl-2 pr-8 py-1 dark:bg-nosferatu-900 transition-all duration-300 ease-in-out transform origin-right ${
+            isInputShow
+              ? 'visible opacity-100 w-full'
+              : 'invisible opacity-0 scale-x-0'
+          }`}
+          value={value}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+          onBlur={() => {
+            if (!value) {
+              setIsInputShow(false);
+            }
+          }}
+          onKeyDown={(event) => {
+            if (event.key === 'Enter') {
+              setSearchInput(value);
+            }
+          }}
+        />
+        <span
+          className={`absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer`}
+          onClick={() => {
+            if (isInputShow) {
+              setSearchInput(value);
+            } else {
+              setIsInputShow(true);
+            }
+          }}
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-          />
-        </svg>
-      </span>
-    </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+            />
+          </svg>
+        </span>
+      </div>
+    </>
   );
 };
 
