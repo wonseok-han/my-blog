f77a4086e77b630f4492647f466af07c7f3574b3
wonseok-han <oshan1112@gmail.com>
2026-04-02 22:11:41 +0900

    update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,8 @@ export const metadata: Metadata = {
   manifest: './manifest.json',
   icons: {
     icon: [
-      { url: '/favicon.svg', type: 'image/svg+xml' },
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon-32.png', sizes: '32x32', type: 'image/png' },
       { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
